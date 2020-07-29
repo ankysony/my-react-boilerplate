992b90cffa417625b3e71bbda7c6be8d96b3a6f9
ankit soni <ankisoni453@gmail.com>
2020-07-29 23:14:49 +0530

    adding buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import PrimaryButton, { SecondaryButton, TertiaryButton } from "./components/Buttons";
-import { GlobalStyle } from "./utils";
+import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
+import { ThemeProvider } from "styled-components";
 
 
-const App = () => (
-  <div>
-        <PrimaryButton>Hello World</PrimaryButton>
-        <SecondaryButton>Ankit soni</SecondaryButton>
-        <TertiaryButton>king of the world</TertiaryButton>
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "90vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <PrimaryButton >Hello World</PrimaryButton>
+        <SecondaryButton>Goodbye World</SecondaryButton>
+        <TertiaryButton>Hey</TertiaryButton>
         <GlobalStyle />
-  </div>
-);
+      </div>
+    </ThemeProvider>
+  );
+};
 
 ReactDOM.render(<App />
      , document.querySelector("#root"));
